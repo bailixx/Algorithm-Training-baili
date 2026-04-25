220427bbbe180ea381b46789f8a741318dcd4c33
bailixx <zx15184279537@qq.com>
2026-04-25 23:48:28 +0800

    update

--- Codeforces/Solutions/codeforces/Cup_2026_Round_1_A.c
+++ Codeforces/Solutions/codeforces/Cup_2026_Round_1_A.c
@@ -1,48 +1,35 @@
 #include <stdio.h>
 
-int gcd (int a, int b)
-{
-    return b == 0 ? a : gcd(b , a % b);
-}
-
-void water()
-{
-    int n;
-    scanf("%d", &n);
-
-    int tree = 0;
-    for (int i = 0; i < n; i++)
-    {
-        int a;
-        scanf("%d", &a);
-
-        int d = 100 / a;
-
-        if (i == 0)
-        {
-            tree = d;
-        }
-        else
-        {
-            tree = gcd(tree, d);
-        }
-    }
-    
-    if (tree == 1) printf("Yes\n");
-
-    else printf("No\n");
-    
-}
-
-int main()
-{
+int main() {
     int t;
     if (scanf("%d", &t) != 1) return 0;
 
-    while(t--)
+    while (t--)
     {
-        water();
-    }
+        int n;
+        scanf("%d", &n);
+        
+        int has100 = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int a;
+            scanf("%d", &a);
 
+            if (a == 100)
+            {
+                has100 = 1;
+            }
+        }
+        
+        if (has100 == 1)
+        {
+            printf("Yes\n");
+        } 
+        else
+        {
+            printf("No\n");
+        }
+    }
+    
     return 0;
 }
