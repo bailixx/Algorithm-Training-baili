c04e18d744352c4bc43a5f561fa12d028c7fe7c5
bailixx <zx15184279537@qq.com>
2026-08-17 17:24:51 +0800

    update

---
+++ Codeforces/Solutions/codeforces/1881A.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h> // strcpy, strcat, strstr 都在这里
+
+void solve()
+{
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    char x[200];
+    char s[50];
+    scanf("%s", x);
+    scanf("%s", s);
+
+    int ops = 0;
+
+    while (ops <= 6)
+    {
+        if (strstr(x, s) != NULL)
+        {
+            printf("%d\n", ops);
+            return;
+        }
+
+        char temp[200];
+        strcpy(temp, x);
+        strcat(x, temp); 
+
+        ops++;
+    }
+
+    printf("-1\n");
+}
+
+int main()
+{
+    int t;
+    if (scanf("%d", &t) != 1) return 0;
+    
+    while (t--)
+    {
+        solve();
+    }
+    
+    return 0;
+}
